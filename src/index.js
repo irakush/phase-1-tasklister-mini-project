7d3395d9cb98a41f5cda067619a509e0bdd0f0c8
Igor Rakush <irakush@aol.com>
2023-11-16 14:50:52 -0500

    add build todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  const inputTask = document.getElementById("new-task-description")
+  console.log(inputTask)
   // your code here
+
+  const form = document.querySelector('form')
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const inputTask = document.getElementById("new-task-description")
+    createToDo(inputTask.value)
+
+    form.reset()
+  })
 });
+
+function createToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let pText = document.createElement('snap')
+
+  btn.addEventListener('click', deleteTask)
+
+  btn.textContent = ' X '
+  // p.textContent = `${todo}`
+  pText.textContent = ` ${todo}`
+  p.appendChild(btn)
+  p.appendChild(pText)
+  
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
